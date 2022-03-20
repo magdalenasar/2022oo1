611fd1dbd73cc8e2419d8d405f75e57bbe3d6277
Magdalena Sarandeva <91531129+magdalenasar@users.noreply.github.com>
2022-03-20 21:18:20 +0100

    Added chapter 4 | Course 2

--- oo_2/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// require statements, get rid of eventually
-require_once __DIR__.'/lib/Ship.php';
-require_once __DIR__.'/lib/BattleManager.php';
-require_once __DIR__.'/lib/ShipLoader.php';
-require_once __DIR__.'/lib/BattleResult.php';
-
